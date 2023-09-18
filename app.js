50ea099c5fa77119f34764bdc6f028937575a020
Lee Mander <lee.mander@gmail.com>
2023-09-18 13:31:45 +0100

    Seperate into different files

---
+++ app.js
@@ -0,0 +1,21 @@
+let name = prompt("What is your name?");
+console.log("User's name is: " + name);
+let food = prompt("What is your favourite food?");
+console.log("User's favourite food is: " + food);
+let transport = prompt("What is your favourite method of transport?");
+console.log("User's preferred method of transport is: " + transport);
+let animal = prompt("What is your favourite animal?");
+console.log("User's favourite animal is: " + animal);
+
+alert(
+  "Welcome, " +
+    name +
+    "! " +
+    food +
+    " is yummy, I agree and " +
+    transport +
+    " is a fine way to get around. " +
+    "Aw, " +
+    animal +
+    "s are so cute!"
+);
